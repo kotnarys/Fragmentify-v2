7eb06baadc0b364b129c4fc734b19864ad6b9b14
3TERRRNITY <qertisww@gmail.com>
2023-02-22 18:59:13 +0300

    add img

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0af9d9d7400c55f6"; }
+/******/ 		__webpack_require__.h = function() { return "665c7499a94f659b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a03a776f75cb84cd.hot-update.js
+++ .next/static/webpack/webpack.a03a776f75cb84cd.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "eb2018b359f1dc4c"; }
+/******/ 	__webpack_require__.h = function() { return "67036ccbaf95be50"; }
 /******/ }();
 /******/ 
 /******/ }
